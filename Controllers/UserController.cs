e7516f40ff2d917fcd44cfe10c55d7fa9af8ff38
samidna <samidna@code.edu.az>
2024-05-14 19:05:58 +0400

    created new project

---
+++ Controllers/UserController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCProject.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Name"] = "Samid";
+            ViewData["Surname"] = "Aghayev";
+            ViewData["Age"] = "25";
+            ViewData["Country"] = "Azerbaijan";
+            ViewData["PhoneNumber"] = "+994554127945";
+            return View();
+        }
+    }
+}
